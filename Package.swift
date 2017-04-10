1ecc3fe6fd06cd75a8a6c16e3839bbee6a841350
Hiroki Kumamoto <kumabook@live.jp>
2017-04-10 12:04:07 +0900

    Use ReactiveSwift

--- Package.swift
+++ Package.swift
@@ -2,9 +2,15 @@ import PackageDescription
 
 let package = Package(
     name: "recordhub",
+    targets: [
+        Target(name: "Lib"),
+        Target(name: "App",  dependencies: [.Target(name: "Lib")]),
+        Target(name: "Task", dependencies: [.Target(name: "Lib")]),
+    ],
     dependencies: [
         .Package(url: "https://github.com/vapor/vapor.git", majorVersion: 1, minor: 5),
         .Package(url: "https://github.com/vapor/postgresql-provider", majorVersion: 1, minor: 0),
+        .Package(url: "https://github.com/ReactiveCocoa/ReactiveSwift.git", majorVersion: 1),
     ],
     exclude: [
         "Config",
