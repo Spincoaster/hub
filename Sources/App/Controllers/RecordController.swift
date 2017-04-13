262d1464a35d45fab4de1c93ccc33050a957fbf9
Hiroki Kumamoto <kumabook@live.jp>
2017-04-13 09:48:25 +0900

    Paginate records view

--- Sources/App/Controllers/RecordController.swift
+++ Sources/App/Controllers/RecordController.swift
@@ -6,20 +6,40 @@ import Lib
 
 final class RecordController: ResourceRepresentable {
     func index(request: Request) throws -> ResponseRepresentable {
-        let query = try Record.query().sort("title", Sort.Direction.ascending)
+        let query      = try Record.query().sort("title", Sort.Direction.ascending)
+        let countQuery = try Record.query()
+        let offset     = request.query?["offset"]?.int ?? 0
+        let limit      = request.query?["limit"]?.int ?? 100
+        var href       = "/records?"
         if let artistId = request.query?["artist_id"]?.int {
+            href += "artist_id=\(artistId)&"
             try query.filter("artist_id", artistId)
+            try countQuery.filter("artist_id", artistId)
         }
         if let userId = request.query?["user_id"]?.int {
+            href += "user_id=\(userId)&"
             try query.filter("user_id", userId)
+            try countQuery.filter("user_id", userId)
         }
-        let records = try query.all()
+        let records = try query.limit(limit, withOffset: offset).all()
         if records.count > 0 {
             let artists = try Artist.query().filter("id", Filter.Scope.in, records.map { $0.artistId }).all()
             let users   = try User.query().filter("id", Filter.Scope.in, records.map { $0.userId }).all()
             Record.setParents(records: records, users: users, artists: artists)
         }
-        let parameters = try Node.object(["records": records.map { try $0.makeLeafNode() }.makeNode()])
+        let count = try countQuery.count()
+        let currentPage = offset / limit
+        let pages = try (0...Int(count / limit)).map { i in
+            return try [
+                "label": "\(i+1)",
+                "active": currentPage == i ? "active" : "",
+                "href": "\(href)offset=\(i * limit)&limit=\(limit)",
+            ].makeNode()
+        }
+        let parameters = try Node.object([
+            "records": records.map { try $0.makeLeafNode() }.makeNode(),
+            "pages": pages.makeNode()
+            ])
         return try drop.view.make("records", parameters)
     }
 
