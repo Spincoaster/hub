0bbfcbe6ee4221ff0f9c10185334075634780815
Hiroki Kumamoto <kumabook@live.jp>
2017-04-15 12:45:53 +0900

    Update dependencies

--- Package.swift
+++ Package.swift
@@ -10,7 +10,7 @@ let package = Package(
     ],
     dependencies: [
         .Package(url: "https://github.com/vapor/vapor.git", majorVersion: 1, minor: 5),
-        .Package(url: "https://github.com/vapor/postgresql-provider", majorVersion: 1, minor: 0),
+        .Package(url: "https://github.com/vapor/postgresql-provider", majorVersion: 1, minor: 1),
         .Package(url: "https://github.com/ReactiveCocoa/ReactiveSwift.git", majorVersion: 1),
     ],
     exclude: [
