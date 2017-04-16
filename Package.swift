3b9b86f98f00e58a7983bcc30cac240f4f100d15
Hiroki Kumamoto <kumabook@live.jp>
2017-04-16 21:18:32 +0900

    Add mecab-swift as dependencies

--- Package.swift
+++ Package.swift
@@ -12,6 +12,7 @@ let package = Package(
         .Package(url: "https://github.com/vapor/vapor.git", majorVersion: 1, minor: 5),
         .Package(url: "https://github.com/vapor/postgresql-provider", majorVersion: 1, minor: 1),
         .Package(url: "https://github.com/ReactiveCocoa/ReactiveSwift.git", majorVersion: 1),
+        .Package(url: "https://github.com/novi/mecab-swift.git", majorVersion: 0, minor: 2),
     ],
     exclude: [
         "Config",
---
+++ Sources/Lib/Array+get.swift
@@ -0,0 +1,9 @@
+//
+//  Array+get.swift
+//  recordhub
+//
+//  Created by Hiroki Kumamoto on 2017/04/16.
+//
+//
+
+import Foundation
