b5d5f191380a61429b563c49a7e6b94f9f3a5d1f
Hiroki Kumamoto <kumabook@live.jp>
2017-06-07 11:32:58 +0900

    Migrate Vapor2: Update Package.swift

--- Package.swift
+++ Package.swift
@@ -6,8 +6,11 @@ let package = Package(
          Target(name: "App"),
     ],
     dependencies: [
-        .Package(url: "https://github.com/vapor/vapor.git", majorVersion: 1, minor: 5),
-        .Package(url: "https://github.com/vapor/postgresql-provider", majorVersion: 1, minor: 1),
+        .Package(url: "https://github.com/vapor/vapor.git", majorVersion: 2),
+        .Package(url: "https://github.com/vapor/fluent-provider.git", majorVersion: 1),
+        .Package(url: "https://github.com/vapor-community/postgresql-provider.git", majorVersion: 2, minor: 0),
+        .Package(url: "https://github.com/vapor/node.git", majorVersion: 2),
+        .Package(url: "https://github.com/vapor/leaf-provider.git", majorVersion: 1),
         .Package(url: "https://github.com/ReactiveCocoa/ReactiveSwift.git", majorVersion: 1),
         .Package(url: "https://github.com/novi/mecab-swift.git", majorVersion: 0, minor: 2),
     ],
