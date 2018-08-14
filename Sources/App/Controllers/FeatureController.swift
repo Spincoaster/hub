06a92faa2a1dc52292cb977cda639a28f66a059b
Hiroki Kumamoto <kumabook@live.jp>
2018-08-14 20:56:47 +0900

    Create and Update feature with `exteranl_link` and `exteranl_thumbnail`

--- Sources/App/Controllers/FeatureController.swift
+++ Sources/App/Controllers/FeatureController.swift
@@ -104,10 +104,12 @@ final class FeatureController: ResourceRepresentable, Pagination, HTMLController
     func update(request: Request, feature: Feature) throws -> ResponseRepresentable {
         if mode != .admin { throw Abort.unauthorized }
         guard let _ = request.currentUser else { return try drop.view.make("error") }
-        let new             = try request.feature()
-        feature.name        = new.name
-        feature.number      = new.number
-        feature.description = new.description
+        let new                   = try request.feature()
+        feature.name              = new.name
+        feature.number            = new.number
+        feature.description       = new.description
+        feature.externalLink      = new.externalLink
+        feature.externalThumbnail = new.externalThumbnail
         try feature.save()
         return try drop.view.make("feature_edit", featureParameters(request: request, feature: feature))
     }
@@ -130,6 +132,8 @@ extension Request {
         guard let name = data["name"]?.string else { throw Abort.badRequest }
         guard let number = data["number"]?.int else { throw Abort.badRequest }
         let description = data["description"]?.string ?? ""
-        return Feature(name: name, number: number, description: description)
+        let externalLink = data["external_link"]?.string ?? ""
+        let externalThumbnail = data["external_thumbnail"]?.string ?? ""
+        return Feature(name: name, number: number, description: description, externalLink: externalLink, externalThumbnail: externalThumbnail)
     }
 }
