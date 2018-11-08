a2f5eb2aeca6c0e3b90e401b793c6448d7c36f96
Hiroki Kumamoto <kumabook@live.jp>
2018-11-08 16:51:43 +0900

    Implement application layout: head and navigation header

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,2 +1,14 @@
 class ApplicationController < ActionController::Base
+  before_action :set_menus
+
+  def set_menus
+    @navigation_menus = [
+      { href: "/top", icon: "home", label:"Top" },
+      { href: "/features", icon: "collections_bookmark", label: "Feature" },
+      { href: "/artists?has_prefix=a", icon: "assignment_ind", label:"Artists" },
+      { href: "/records?has_prefix=a", icon: "album", label:"Records" },
+      { href: "/tracks?has_prefix=a" , icon: "high_quality", label: "Hi-Res" },
+      { href: "/owners" , icon: "perm_identity", label: "Owners" }
+    ]
+  end
 end
