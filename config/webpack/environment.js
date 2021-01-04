8832eaf197cc423c345289021d0d26459e18bf90
Hiroki Kumamoto <kumabook@live.jp>
2021-01-04 17:33:07 +0900

    Update webpacker config

--- config/webpack/environment.js
+++ config/webpack/environment.js
@@ -1,3 +1,12 @@
-const { environment } = require('@rails/webpacker')
+const { environment } = require('@rails/webpacker');
 
-module.exports = environment
+const sassLoaderIndex = environment.loaders
+  .get("sass")
+  .use.findIndex(el => el.loader === "sass-loader");
+
+let sassLoader = environment.loaders.get("sass").use[sassLoaderIndex];
+sassLoader.options.includePaths = ["./node_modules"];
+
+environment.loaders.get("sass").use[sassLoaderIndex] = sassLoader;
+
+module.exports = environment;
