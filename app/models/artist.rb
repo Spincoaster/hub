c392645406480abd9c1adcd8c27fed78ae3614ac
Hiroki Kumamoto <kumabook@live.jp>
2021-01-06 13:52:06 +0900

    Delete child records when delete an artist

--- app/models/artist.rb
+++ app/models/artist.rb
@@ -1,6 +1,6 @@
 class Artist < ApplicationRecord
   include NameSearchable
-  has_many :records
+  has_many :records, dependent: :destroy
   has_many :albums, dependent: :destroy
   has_many :tracks, dependent: :destroy
 
