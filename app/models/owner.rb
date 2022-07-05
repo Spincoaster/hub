d6cb07081dabc80eed63adf5f14e710cf0666faf
Hiroki Kumamoto <kumabook@live.jp>
2022-07-05 11:35:28 +0900

    Fix crash of owner search

--- app/models/owner.rb
+++ app/models/owner.rb
@@ -1,4 +1,17 @@
 class Owner < ApplicationRecord
   include NameSearchable
   has_many :records, dependent: :destroy
+
+  scope :search_with_prefix , ->(v) {
+    query = nil
+    letters = NameSearchable::prefix_letters(v)
+    letters.chars do |c|
+      if query.nil?
+        query = where("name LIKE ?", "#{c}%")
+      else
+        query = query.or(where("name LIKE ?", "#{c}%"))
+      end
+    end
+    query
+  }
 end
