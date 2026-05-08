0e8bbf594786b3c68cfab11d025667783836152f
kumabook <kumabook@live.jp>
2026-05-08 20:49:11 +0900

    chore: update nextjs security patches

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -13,6 +13,11 @@ const eslintConfig = defineConfig([
     "build/**",
     "next-env.d.ts",
   ]),
+  {
+    rules: {
+      "react-hooks/set-state-in-effect": "off",
+    },
+  },
 ]);
 
 export default eslintConfig;
